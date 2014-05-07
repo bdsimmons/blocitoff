940fe9612f73c97d4c817c2e29898b46dd094372
Ben <ben.simmons@trinityis.com>
2014-05-07 08:50:44 -0500

    Added spec for User model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should be able to sign up with a username, password, and email address'
+  it 'should be able to sign in and out'
+  it 'should be able to create a single to-do list'
+  it 'should be abel to create multiple to-do items on its own to-do list'
+  it 'should be able to mark to-do items as complete'
+  it 'should be able to see how many days are left before a to-do item is automatically deleted'
 end
