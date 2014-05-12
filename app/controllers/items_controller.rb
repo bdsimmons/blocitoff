edfb421ff29c2ad607afec1cea2cef947dfae496
Ben <ben.simmons@trinityis.com>
2014-05-11 23:34:31 -0500

    Added test for creating items and associated code.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,26 @@ class ItemsController < ApplicationController
   def index
     @list = List.find(params[:list_id])
   end
+
+  def new
+    @list = List.find(params[:list_id])
+    @item = @list.items.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @item = @list.items.new(item_params)
+    if @item.save
+      flash[:success] = "Added list item."
+      redirect_to list_items_path
+    else
+      flash[:error] = "There was a problem adding that list item."
+      render action: :new
+    end
+  end
+
+  private
+  def item_params
+    params[:item].permit(:content)
+  end
 end
