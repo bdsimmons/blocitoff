62961e64fe7b8c1488be6df7c0b684a8c68fbc64
Ben <ben.simmons@trinityis.com>
2014-05-13 11:41:09 -0500

    Add specs and code for editing list items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,27 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @list = List.find(params[:list_id])
+    @item = @list.items.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:list_id])
+    @item = @list.items.find(params[:id])
+    if @item.update_attributes(item_params)
+      flash[:success] = "Saved list item."
+      redirect_to list_items_path
+    else
+      flash[:error] = "That list item could not be saved."
+      render action: :edit
+    end
+  end
+
+  def url_options
+    { list_id: params[:list_id] }.merge(super)
+  end
+
   private
   def item_params
     params[:item].permit(:content)
--- spec/features/items/create_spec.rb
+++ spec/features/items/create_spec.rb
@@ -20,4 +20,29 @@ describe "Adding items" do
       expect(page).to have_content("Milk")
     end
   end
+
+  it "displays an error with no content" do
+    visit_list(list)
+    click_link "New Item"
+    fill_in "Content", with: ""
+    click_button "Save"
+
+    within("div.flash") do
+      expect(page).to have_content("There was a problem adding that list item.")
+    end
+    expect(page).to have_content("Content can't be blank")
+  end
+
+  it "displays an error with content less than 2 characters long" do
+    visit_list(list)
+    click_link "New Item"
+    fill_in "Content", with: "1"
+    click_button "Save"
+
+    within("div.flash") do
+      expect(page).to have_content("There was a problem adding that list item.")
+    end
+    expect(page).to have_content("Content is too short")
+  end
+
 end
